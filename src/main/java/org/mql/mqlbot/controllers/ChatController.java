d82d113b774df9b01a4b74e9be532b896c555c6d
Abdelhamid Fadel <fadelabdelhamid1@gmail.com>
2024-12-07 22:29:32 +0100

    added test controller

--- src/main/java/org/mql/mqlbot/controllers/ChatController.java
+++ src/main/java/org/mql/mqlbot/controllers/ChatController.java
@@ -5,6 +5,7 @@ import org.mql.mqlbot.services.AiChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,11 @@ public class ChatController {
 
     public ChatController(@Autowired AiChatService aiChatService) {
         this.aiChatService = aiChatService;
+    }
+
+    @GetMapping("/")
+    public String test(){
+        return "Hello, the app is running !";
     }
 
     @PostMapping("/chat")
