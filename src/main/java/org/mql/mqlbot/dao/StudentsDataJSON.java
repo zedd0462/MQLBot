147d1f1d8f922b5614b8c2817e64a925201e09ca
Abdelhamid Fadel <fadelabdelhamid1@gmail.com>
2024-12-08 13:44:06 +0100

    changed data path

--- src/main/java/org/mql/mqlbot/dao/StudentsDataJSON.java
+++ src/main/java/org/mql/mqlbot/dao/StudentsDataJSON.java
@@ -2,8 +2,11 @@ package org.mql.mqlbot.dao;
 
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 
 
@@ -11,6 +14,12 @@ import java.util.List;
 public class StudentsDataJSON implements StudentsData {
     @Override
     public List<Document> getStudentsData() {
-        return FileSystemDocumentLoader.loadDocuments("src/main/resources/data");
+        try {
+            Path directoryPath = new ClassPathResource("data").getFile().toPath();
+            return FileSystemDocumentLoader.loadDocuments(directoryPath);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load data directory", e);
+        }
+
     }
 }
