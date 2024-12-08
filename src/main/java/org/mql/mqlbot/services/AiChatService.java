797c5b28b8e2a99ed1aa8cbd783704418f657cf6
Abdelhamid Fadel <fadelabdelhamid1@gmail.com>
2024-12-08 12:00:49 +0100

    removed unused imports

--- src/main/java/org/mql/mqlbot/services/AiChatService.java
+++ src/main/java/org/mql/mqlbot/services/AiChatService.java
@@ -1,10 +1,7 @@
 package org.mql.mqlbot.services;
 
 import dev.langchain4j.service.SystemMessage;
-import dev.langchain4j.service.spring.AiService;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface AiChatService {
     @SystemMessage("You are a polite assistant")
     String chat(String userMessage);
--- src/main/java/org/mql/mqlbot/services/RAGService.java
+++ src/main/java/org/mql/mqlbot/services/RAGService.java
@@ -1,7 +1,6 @@
 package org.mql.mqlbot.services;
 
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
-import org.springframework.stereotype.Service;
 
 public interface RAGService {
     ContentRetriever getContentRetriever();
