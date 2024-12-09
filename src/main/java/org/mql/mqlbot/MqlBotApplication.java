4819ddae833de31bb52fcc5753add323c9b79d10
Abdelhamid Fadel <fadelabdelhamid1@gmail.com>
2024-12-09 16:53:40 +0100

    Removed unused imports

--- src/main/java/org/mql/mqlbot/MqlBotApplication.java
+++ src/main/java/org/mql/mqlbot/MqlBotApplication.java
@@ -1,29 +1,11 @@
 package org.mql.mqlbot;
 
-import dev.langchain4j.data.document.Document;
-import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
-import dev.langchain4j.data.segment.TextSegment;
-import dev.langchain4j.memory.chat.MessageWindowChatMemory;
-import dev.langchain4j.model.openai.OpenAiChatModel;
-import dev.langchain4j.model.openai.OpenAiChatModelName;
-import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
-import dev.langchain4j.service.AiServices;
-import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
-import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
-import org.mql.mqlbot.services.AiChatService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 @SpringBootApplication
 public class MqlBotApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MqlBotApplication.class, args);
     }
-
-
 }
