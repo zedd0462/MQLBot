ba9b58b96e7d6966243be0a9bfc70f3cefd62c9e
Abdelhamid Fadel <fadelabdelhamid1@gmail.com>
2024-12-09 19:51:07 +0100

    Added Date to the system prompt.

--- src/main/java/org/mql/mqlbot/services/AiChatService.java
+++ src/main/java/org/mql/mqlbot/services/AiChatService.java
@@ -3,6 +3,6 @@ package org.mql.mqlbot.services;
 import dev.langchain4j.service.SystemMessage;
 
 public interface AiChatService {
-    @SystemMessage("Vous êtes un assistant qui aide à la gestion des informations sur les étudiants MQL. Vous recevrez une prompt et une question, essayez de répondre à la prompt avec les données si nécessaire, soyez concis et précis.")
+    @SystemMessage("Nous sommes dans Dec 2024, Vous êtes un assistant qui aide à la gestion des informations sur les étudiants MQL. Vous recevrez une prompt et une question, essayez de répondre à la prompt avec les données si nécessaire, soyez concis et précis.")
     String chat(String userMessage);
 }
